bb55d8c11382ec6d3acb3f6671063b2e7d1e965e
renin.guo <renbin.guo@jci.com>
2019-08-21 09:46:24 +0800

    [add] 开发登陆功能2 ：读取sqlite数据库成功
    note:
    配置了如下2个url:
    
    登陆的网址：http://127.0.0.1:8000/hotel/login/
    网址首页：http://127.0.0.1:8000/hotel/

--- backendApi/hotel/helperCollection.py
+++ backendApi/hotel/helperCollection.py
@@ -1,4 +1,15 @@
 import sqlite3
+import os
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+HOTEL_LOG_DIR = './logs/hotelLogs.txt'  #在manager.py目录下的logs
+
+########################################################################################################
+#        brief: sqlite3的辅助class
+#        input:
+#        returns:    
+########################################################################################################
 class sqliteOperate:  
     def __init__(self,dbfile='hotel.sqlite3'): 
         try:  
@@ -8,8 +19,52 @@ class sqliteOperate:
             print ('连接数据库出错：')  
             print (e)  
       
-    def execute(self,sql):
-		rows = self.cursor.execute(sql)
-		return rows
-def justTest():
-	return 'ehie'
+    def select(self,sql):
+        self.cursor.execute(sql)
+        rows = self.cursor.fetchall()  
+        return rows
+
+
+########################################################################################################
+#        brief: 打印log的辅助函数
+#        input:  无
+#        return: 无 
+#         note:     1.log路径由全局变量HOTEL_LOG_DIR变量指定
+#                   2.日志级别 ：
+#                        日志等级(从高到低)：CRITICAL->ERROR->WARNING->INFO->DEBUG
+#                        一旦设置了日志等级，则调用比等级低的日志记录函数则不会输出
+########################################################################################################
+def get_logger():
+    #set up logging to file
+    logs_dir = os.path.join(os.path.curdir, "logs")
+    if os.path.exists(logs_dir) and os.path.isdir(logs_dir):
+        pass
+    else:
+        os.mkdir(logs_dir)
+    # 向文件输出
+    # 仅保留最近三天的日志 
+    rotatingFileHandler = logging.handlers.TimedRotatingFileHandler(filename = HOTEL_LOG_DIR, when="D", interval=1, backupCount=3)
+    #for test   10创建一个新log文件,最多保留2个问题log文件
+    #rotatingFileHandler = logging.handlers.TimedRotatingFileHandler(filename="./logs/log_weather.txt", when="S", interval=10, backupCount=2) 
+
+    formatter = logging.Formatter('%(asctime)s   %(levelname)-8s %(filename)s line: %(lineno)s   [%(funcName)s]   %(message)s')
+    rotatingFileHandler.setFormatter(formatter)
+    logging.getLogger("").addHandler(rotatingFileHandler)
+
+    # 向屏幕输出
+    console = logging.StreamHandler()
+    console.setLevel(logging.NOTSET)
+    formatter = logging.Formatter('%(asctime)s   %(levelname)-8s %(filename)s line: %(lineno)s   [%(funcName)s]   %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger("").addHandler(console)
+
+    # set initial log level
+    logger = logging.getLogger("")
+    
+
+    # # 日志等级(从高到低)：CRITICAL->ERROR->WARNING->INFO->DEBUG
+    # # 一旦设置了日志等级，则调用比等级低的日志记录函数则不会输出
+    logger.setLevel(logging.DEBUG)  ## 调试时可设置为DEBUG
+
+    return logger
+logger = get_logger()
--- backendApi/hotel/views.py
+++ backendApi/hotel/views.py
@@ -1,42 +1,25 @@
 from django.shortcuts import render
-
+from rest_framework.parsers import JSONParser
+import json
 # Create your views here.
 from django.conf.urls import url
-from helperCollection import sqliteOperate,justTest
-
 from . import views
 from django.http import HttpResponse
+from hotel.helperCollection import sqliteOperate,logger # 这里必须加上hotel 不然会报错
 import sqlite3
 
 def hotel_index(request):
-    justTest()
+    logger.info("## 欢迎访问我的博客首页！")
     return HttpResponse("欢迎访问我的博客首页！")
 
-
 def hotel_login(request):
-    conn = sqlite3.connect('hotel.sqlite3')
-    print("Opened database successfully")
-    return HttpResponse("欢迎登陆！")
+    print("##### hotel_login")
     db = sqliteOperate('hotel.sqlite3')
-    sql = 'select *from user;'
-    rows = db.execute(sql)
+    rows = db.select("select * from user")
+    print("rows = ")
     print(rows)
-    # if request.method == 'GET':
-    #     sql = "select * from user"
-    #     helper = DBHelper()
-    #     helper.setDB("hotel.sqlite3")
-    #     rows= helper.queryAll(sql)
-    #     data=json.dumps(rows, cls=DecimalEncoder)
-    #     if len(data)>0:
-    #         return JSONResponseEx(data, status = 201)
-    #     return JSONResponseEx("出错",status=400)
-    # if request.method == 'POST':
-    #     requestData = JSONParser().parse(request)
-    #     helper=DBHelper()
-    #     helper.setDB("seed_building_db")
-    #     sql = "update seed_building_db.tbl_setting set svalue = '%s' where skey = 'electricityprice';"%(requestData["elecPrice"])
-    #     rows= helper.queryAll(sql)
-    #     data=json.dumps(rows)
-    #     if len(data)>0:
-    #         return JSONResponseEx(data, status = 201)
-    #     return JSONResponseEx("出错",status=400)
+    # requestData = JSONParser().parse(request)
+    # print(requestData)
+    return HttpResponse("欢迎登陆!")
+
+   
