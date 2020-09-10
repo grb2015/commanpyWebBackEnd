6e9e80d48529b0bc33908c0dce65b3756e1822dc
grb2015 <949409706@qq.com>
2020-09-10 11:56:41 +0800

    [add] 添加数据库文件

---
+++ database.sql
@@ -0,0 +1,79 @@
+CREATE DATABASE  IF NOT EXISTS `grb_farmhouse_db` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+USE `grb_farmhouse_db`;
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: 139.196.205.73    Database: grb_farmhouse_db
+-- ------------------------------------------------------
+-- Server version	5.7.13
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_user_info`
+--
+
+DROP TABLE IF EXISTS `tbl_user_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_user_info` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(45) COLLATE utf8_bin DEFAULT NULL COMMENT '会员名',
+  `user_level_id` int(11) DEFAULT NULL COMMENT '会员等级id \n关联到 tbl_user_level 表',
+  `user_password` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='保存会员的信息';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_user_info`
+--
+
+LOCK TABLES `tbl_user_info` WRITE;
+/*!40000 ALTER TABLE `tbl_user_info` DISABLE KEYS */;
+INSERT INTO `tbl_user_info` VALUES (1,'luoxiao',4,'123456'),(2,'xiaoming',1,'123456');
+/*!40000 ALTER TABLE `tbl_user_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_user_level`
+--
+
+DROP TABLE IF EXISTS `tbl_user_level`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_user_level` (
+  `user_level_id` int(11) NOT NULL,
+  `user_level_name` varchar(45) COLLATE utf8_bin DEFAULT NULL COMMENT '会员等级',
+  PRIMARY KEY (`user_level_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='会员等级表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_user_level`
+--
+
+LOCK TABLES `tbl_user_level` WRITE;
+/*!40000 ALTER TABLE `tbl_user_level` DISABLE KEYS */;
+INSERT INTO `tbl_user_level` VALUES (1,'铜卡会员'),(2,'银卡会员'),(3,'金卡会员'),(4,'钻石会员');
+/*!40000 ALTER TABLE `tbl_user_level` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-09-10 11:34:31
